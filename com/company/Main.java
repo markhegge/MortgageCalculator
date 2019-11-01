c8c827db97b0bd0c665ab7e1c39253cfbab890ce
markhegge <hegge.mark@gmail.com>
2019-11-01 06:54:57 -0700

    updated formatting

--- com/company/Main.java
+++ com/company/Main.java
@@ -8,25 +8,19 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
-
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Principal: ");
         int principal = scanner.nextInt();
-
         System.out.print("Interest Rate: ");
         float annualInterest = scanner.nextFloat();
-
         System.out.print("How many years: ");
         int YEARS = scanner.nextInt();
-
         int MONTHS = YEARS * MONTHS_IN_YEAR;
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
         int numberOfPayments = YEARS * MONTHS_IN_YEAR;
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments ) -1);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-
         System.out.print("Mortgage: " + mortgageFormatted);
 
     }
